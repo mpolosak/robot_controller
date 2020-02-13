800a332b81843fbcb979bb93a0fb9a05f7e78d8a
256Michael <256Michal@gmail.com>
2020-02-13 13:43:58 +0100

    Add AppBar to RobotControllerHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class _RobotControllerHomePageState extends State<RobotControllerHomePage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-
+      appBar: AppBar(title: Text(widget.title),),
     );
   }
 }
