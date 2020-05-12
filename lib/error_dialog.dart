0be179e1f6f6319bc87090f2ddd288a8cc9e692a
mpolosak <42770518+mpolosak@users.noreply.github.com>
2020-05-12 19:06:07 +0200

    Move showing dialog to new function show_error_dialog

---
+++ lib/error_dialog.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void showErrorDialog(BuildContext context, String title, String errorMessage, {bool fatal=false})
+{
+  showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(errorMessage),
+        actions: <Widget>[
+          fatal?
+            FlatButton(
+              child: Text('Close app'),
+              onPressed: () {
+                SystemNavigator.pop();
+              },
+            )
+          :
+          FlatButton(
+            child: Text('Ok'),
+            onPressed: (){
+              Navigator.of(context).pop();
+            }
+          )
+        ],
+      );
+    }
+  );
+}
